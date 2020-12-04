5d3ba3390b651eb344d2d15b04e8c464ff829a78
greatbridf <guobingfang20040616@gmail.com>
2020-12-04 20:13:46 +0800

    add tests

--- test/test_run.cpp
+++ test/test_run.cpp
@@ -1,6 +1,22 @@
 #include <gbase/gbase.hpp>
 
+#include <iostream>
+
+class A : public interface {
+ public:
+    void fuck();
+};
+
+void A::fuck() {
+    std::cout << "fuckyou" << std::endl;
+}
+
 int main() {
     gb::greetings();
+
+    A fuck;
+
+    fuck.fuck();
+
     return 0;
 }
